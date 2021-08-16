e68dd7063e69b7070b5ccaf167b38fb5f5f24015
Aaron Yu <ayybuddyy@gmail.com>
2021-08-16 17:55:26 -0400

    Update config file

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoiYXlidWRkeSIsImEiOiJja3Nld3pya24xNHY0MnBvMGZzcndsYTdpIn0.NR6nPLlW69JKh8SgVsLemg',
+  },
 };
