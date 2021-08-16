a9c6de941ae52bc5205f0fc1567305ab551b8f09
Aaron Yu <ayybuddyy@gmail.com>
2021-08-16 13:24:47 -0400

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   images: {
     domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoiYXlidWRkeSIsImEiOiJjazFmeHZ3ZXIwdW83M2hwN2wxbDl6cXc1In0.WNybvv_GiC232d1Dzl6LiQ',
   },
 };
